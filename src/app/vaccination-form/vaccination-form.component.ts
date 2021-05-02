d5783395cdbdc06e592edfc075dfb51c9450ad53
lisamillinger <s1810456020@students.fh-hagenberg.at>
2021-05-02 21:05:16 +0200

    1 file modified, 1 added

--- src/app/vaccination-form/vaccination-form.component.ts
+++ src/app/vaccination-form/vaccination-form.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, FormArray, Validators, FormControl
+} from "@angular/forms";
+import {VaccinationFactory } from "../shared/vaccination-factory";
+import { VaccinationStoreService } from "../shared/vaccination-store.service";
+import { Vaccination, Location } from "../shared/vaccination";
 
 @Component({
   selector: 'app-vaccination-form',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vaccination-form.component.css']
 })
 export class VaccinationFormComponent implements OnInit {
+  vaccinationForm: FormGroup;
+  vaccination = VaccinationFactory.empty();
+  errors: { [key: string]: string} = {};
+  isUpdatingVaccination = false;
+  locations: FormArray;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private app: VaccinationStoreService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    const key = this.route.snapshot.params["key"];
+    if(key) {
+      this.isUpdatingVaccination = true;
+      this.app.getSingle(key).subscribe(vaccination => {this.vaccination = vaccination; this.initVaccination();
+      });
+    }
+    this.initVaccination();
+  }
+
+  initVaccination() {
+    this.vaccinationForm = this.fb.group({
+      
+    })
+
+  }
+
+  submitForm() {
+
   }
 
 }
