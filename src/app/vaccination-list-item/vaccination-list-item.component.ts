fa3fa0b033577b72c0d215f2cece2af526af3668
lisamillinger <s1810456020@students.fh-hagenberg.at>
2021-05-02 17:04:26 +0200

    2 files modified

--- src/app/vaccination-list-item/vaccination-list-item.component.ts
+++ src/app/vaccination-list-item/vaccination-list-item.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {Vaccination} from "../shared/vaccination";
+import { Component, OnInit, Input } from "@angular/core";
+import { Vaccination } from "../shared/vaccination";
 
 @Component({
-  selector: 'a.app-vaccination-list-item',
-  templateUrl: './vaccination-list-item.component.html',
-  styleUrls: ['./vaccination-list-item.component.css']
+  selector: "a.app-vaccination-list-item",
+  templateUrl: "./vaccination-list-item.component.html",
+  styleUrls: ["./vaccination-list-item.component.css"]
 })
 export class VaccinationListItemComponent implements OnInit {
-  @Input() vaccination: Vaccination
+  @Input() vaccination: Vaccination;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.vaccination);
   }
-
 }
