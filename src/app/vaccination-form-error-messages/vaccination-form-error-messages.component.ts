bb5b1b99527d9c26d785143dd9bf9464db2a3bdf
lisamillinger <s1810456020@students.fh-hagenberg.at>
2021-05-04 12:37:42 +0200

    1 file modified, 1 added

--- src/app/vaccination-form-error-messages/vaccination-form-error-messages.component.ts
+++ src/app/vaccination-form-error-messages/vaccination-form-error-messages.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-vaccination-form-error-messages',
-  templateUrl: './vaccination-form-error-messages.component.html',
-  styleUrls: ['./vaccination-form-error-messages.component.css']
+  selector: "app-vaccination-form-error-messages",
+  templateUrl: "./vaccination-form-error-messages.component.html",
+  styleUrls: ["./vaccination-form-error-messages.component.css"]
 })
 export class VaccinationFormErrorMessagesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
