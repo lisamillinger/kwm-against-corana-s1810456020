e3452c53be68c55cdbcad3349535969bc75dbdb1
lisamillinger <s1810456020@students.fh-hagenberg.at>
2021-05-06 18:49:45 +0200

    PersonenId kann ausgelesen werden

--- src/app/vaccination-details/vaccination-details.component.ts
+++ src/app/vaccination-details/vaccination-details.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, Input, Output } from "@angular/core";
-import { Vaccination } from "../shared/vaccination";
-import { People } from "../shared/people";
-import { VaccinationStoreService } from "../shared/vaccination-store.service";
-import { ActivatedRoute, Router } from "@angular/router";
-import { VaccinationFactory } from "../shared/vaccination-factory";
-import { AuthenticationService } from "../shared/authentication.service";
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { Vaccination } from '../shared/vaccination';
+import { People } from '../shared/people';
+import { VaccinationStoreService } from '../shared/vaccination-store.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VaccinationFactory } from '../shared/vaccination-factory';
+import { AuthenticationService } from '../shared/authentication.service';
 
 @Component({
-  selector: "app-vaccination-details",
-  templateUrl: "./vaccination-details.component.html",
-  styleUrls: ["./vaccination-details.component.css"]
+  selector: 'app-vaccination-details',
+  templateUrl: './vaccination-details.component.html',
+  styleUrls: ['./vaccination-details.component.css']
 })
 export class VaccinationDetailsComponent implements OnInit {
   vaccination: Vaccination = VaccinationFactory.empty();
@@ -25,15 +25,15 @@ export class VaccinationDetailsComponent implements OnInit {
     console.log(this.vaccination.people);
     console.log(this.vaccination.people.length);
     const params = this.route.snapshot.params;
-    this.app.getSingle(params["key"]).subscribe(v => (this.vaccination = v));
+    this.app.getSingle(params['key']).subscribe(v => (this.vaccination = v));
   }
 
   removeVaccination() {
-    if (confirm("Impfung wirklich löschen?")) {
+    if (confirm('Impfung wirklich löschen?')) {
       this.app
         .remove(this.vaccination.key)
         .subscribe(res =>
-          this.router.navigate(["../"], { relativeTo: this.route })
+          this.router.navigate(['../'], { relativeTo: this.route })
         );
     }
   }
@@ -47,6 +47,7 @@ export class VaccinationDetailsComponent implements OnInit {
   }
 
   getsVaccinated(e) {
-
+    console.log('hier ist etwas passiert');
+    console.log(e);
   }
 }
