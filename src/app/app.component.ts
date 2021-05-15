0f6f3b9483a7a23db830bb98de9f66cc86b87152
lisamillinger <s1810456020@students.fh-hagenberg.at>
2021-05-15 14:28:21 +0200

    almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, VERSION } from "@angular/core";
-import { Vaccination } from "./shared/vaccination";
-import { AuthenticationService } from "./shared/authentication.service";
+import { Component, VERSION } from '@angular/core';
+import { Vaccination } from './shared/vaccination';
+import { AuthenticationService } from './shared/authentication.service';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   constructor(private authService: AuthenticationService) {}
@@ -15,6 +15,17 @@ export class AppComponent {
   }
 
   getLoginLabel() {
-    return this.isLoggedIn() ? "Logout" : "Login";
+    return this.isLoggedIn() ? 'Logout' : 'Login';
+  }
+
+  isAdmin() {
+    console.log('in isamdin');
+    let isAdmin = sessionStorage.getItem('isAdmin');
+    if (isAdmin == 'true') {
+      return true;
+    } else {
+      console.log('not an admin');
+      return false;
+    }
   }
 }
--- src/app/shared/authentication.service.ts
+++ src/app/shared/authentication.service.ts
@@ -1,21 +1,22 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import jwt_decode from "jwt-decode";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
 
 interface Token {
   exp: number;
   people: {
     id: string;
+    isAdmin: boolean;
+    sv_number: string;
   };
 }
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
-
 export class AuthenticationService {
   private api: string =
-    "https://corana21.s1810456020.student.kwmhgb.at/api/auth";
+    'https://corana21.s1810456020.student.kwmhgb.at/api/auth';
 
   constructor(private http: HttpClient) {}
 
@@ -27,38 +28,63 @@ export class AuthenticationService {
   }
 
   public setLocalStorage(token: string) {
-    console.log("storing stoken");
+    console.log('storing stoken');
     const decodedToken = jwt_decode(token) as Token;
     console.log(decodedToken.people.id);
+    console.log(decodedToken.people.isAdmin);
+    console.log(decodedToken.people.sv_number);
 
-    sessionStorage.setItem("token", token);
-    sessionStorage.setItem("userId", decodedToken.people.id);
+    sessionStorage.setItem('token', token);
+    sessionStorage.setItem('sv_number', decodedToken.people.sv_number);
+    sessionStorage.setItem('userId', decodedToken.people.id);
+    if (decodedToken.people.isAdmin == false) {
+      sessionStorage.setItem('isAdmin', 'false');
+    } else {
+      sessionStorage.setItem('isAdmin', 'true');
+    }
   }
 
   public getCurrentUserId() {
-    return Number.parseInt(sessionStorage.getItem("userId"));
+    return Number.parseInt(sessionStorage.getItem('userId'));
+  }
+
+  public getCurrentAdmin() {
+    return localStorage.getItem('isAdmin');
   }
 
   logout() {
     this.http.post(`${this.api}/logout`, {});
-    sessionStorage.removeItem("token");
-    sessionStorage.removeItem("userId");
-    console.log("logged out");
+    sessionStorage.removeItem('sv_number');
+    sessionStorage.removeItem('isAdmin');
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('userId');
+    console.log('logged out');
   }
 
   public isLoggedIn() {
-    if (sessionStorage.getItem("token")) {
-      let token: string = sessionStorage.getItem("token");
+    if (sessionStorage.getItem('token')) {
+      let token: string = sessionStorage.getItem('token');
       const decodedToken = jwt_decode(token) as Token;
       let expirationDate: Date = new Date(0);
       expirationDate.setUTCSeconds(decodedToken.exp);
       if (expirationDate < new Date()) {
-        console.log("token expired");
-        sessionStorage.removeItem("token");
+        console.log('token expired');
+        sessionStorage.removeItem('token');
         return false;
       }
       return true;
     } else {
+      return false;
+    }
+  }
+
+  public isAdmin() {
+    console.log("in isamdin");
+    let isAdmin = sessionStorage.getItem('isAdmin');
+    if (isAdmin == "true") {
+      return true;
+    } else {
+      console.log("not an admin");
       return false;
     }
   }
